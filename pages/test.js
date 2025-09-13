997bcdf8c43ae65bbe88b84a581dd4993372a26e
hr <hr@gmail.com>
2025-09-13 11:34:19 +0530

    Added OTP authentication and route protection

--- pages/test.js
+++
@@ -1,8 +0,0 @@
-export default function Test() {
-  return (
-    <div style={{ fontFamily: "Arial", padding: "20px" }}>
-      <h1>Vercel Test Page</h1>
-      <p>If you see this, Vercel is deploying the latest commit correctly.</p>
-    </div>
-  );
-}
